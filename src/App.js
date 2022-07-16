5917b0d3bfe39ac105027fab4812dc5ef608cc80
sanket <wankhdes29@gmail.com>
2022-07-16 11:30:30 +0530

    done assignmet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Expenses from "./components/Expenses";
-import "./components/Expenses.css";
 
 function App() {
   const expense = [
@@ -27,34 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-
-      <div className="expenses">
-        <Expenses expense={expense[0]}></Expenses>
-      </div>
-
-      <div className="expenses">
-        <Expenses expense={expense[1]}></Expenses>
-      </div>
-
-      <div className="expenses">
-        <Expenses expense={expense[2]}></Expenses>
-      </div>
-
-      {/* <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      ></ExpenseItem> */}
+      <Expenses items={expense}></Expenses>
     </div>
   );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,33 @@
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 function Expenses(props) {
   console.log(props);
   return (
-    <ExpenseItem
-      title={props.expense.title}
-      amount={props.expense.amount}
-      date={props.expense.date}
-    ></ExpenseItem>
+    <div className="expenses ">
+      return(
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      ></ExpenseItem>
+      )
+    </div>
   );
 }
 
